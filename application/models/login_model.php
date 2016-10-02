0be7336d1ca70625877ee535c99969dd18c91bfb
yashanthy <yashanthy93@gmail.com>
2016-10-02 12:33:39 -0500

    add

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-Class login extends CI_Model
+Class login_model extends CI_Model
 {
     public  function _construct()
     {
